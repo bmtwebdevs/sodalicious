57b97840527d22b958dabe7d6824b107c6756b92
Luke McHale-Jones <LukeMcJones@live.co.uk>
2017-06-12 16:22:11 +0100

    Added React Application boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+import AppContainer from './containers/appContainer';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <AppContainer />
+    </Provider>,
+    document.getElementById('root'));
+
 registerServiceWorker();
