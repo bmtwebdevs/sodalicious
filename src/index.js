366a958e479ebfea361343ff9b22c2c897d5a0c8
jeremy linter <jeremy linter>
2017-06-13 14:10:21 +0100

    Update webpack.config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import AppContainer from './containers/appContainer';
 import registerServiceWorker from './registerServiceWorker';
+import "bootstrap/dist/css/bootstrap.css";
 import './index.css';
 
 ReactDOM.render(
