9dee9afe05da1b21c0d0e62e50efe577f66fd956
jeremy linter <jeremy linter>
2017-06-13 14:38:54 +0100

    Update Hero

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -11,7 +11,7 @@ class App extends Component {
         <div className="jumbotron">
           <h1>#Sodalicious</h1>
           <p>An Alexa controlled smart drinks mixer for the technogeek-connoisseur powered by <b>British Mixology Technology</b></p>
-          <p><a className="btn btn-primary btn-lg" href="#" role="button">Make a drink</a></p>
+          <p><a className="btn btn-danger btn-lg" href="#" role="button">Make a drink</a></p>
         </div>
         <DrinksCatalog />
       </div>
