ca62573b96fdd9f82250774a945ae0c8f101768f
Richard Caunt <richardcaunt@gmail.com>
2017-06-14 12:01:44 +0100

    Added routing to client

---
+++ client/src/NotFound.js
@@ -0,0 +1,8 @@
+// modules/ADmin.js
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return <div>Not Found SUCKER</div>
+  }
+})
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import '../semantic/dist/semantic.min.css';
+import Admin from './Admin'
+import Home from './Home'
 
-ReactDOM.render(
-  <App />,
+import NotFound from './NotFound'
+
+import { BrowserRouter, Route, IndexRoute, Switch } from 'react-router-dom'
+
+ReactDOM.render((
+    <BrowserRouter>
+      <App>
+        <Switch>
+        <Route exact={true} path="/" component={Home}/>
+        <Route path="/admin" component={Admin}/>
+        <Route path="*" component={NotFound}/>
+        </Switch>
+      </App>
+    </BrowserRouter>),
   document.getElementById('root'), // eslint-disable-line no-undef
 );
