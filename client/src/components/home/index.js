cca6b85d07657b52eafecdd43db4990b42bdb131
Luke McHale-Jones <LukeMcJones@live.co.uk>
2017-06-15 08:55:23 +0100

    Fixing the index

---
+++ client/src/components/home/index.js
@@ -0,0 +1,22 @@
+import Hero from './components/hero';
+import DrinkSelector from './components/drinkSelector';
+import Footer from './components/footer';
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+class Home extends Component {
+ 
+  render() {
+    return (
+      <div className='Home'>
+        <Hero/>
+        <DrinkSelector/>
+        <div className='ui text container'>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default Home;
