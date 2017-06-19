f784011e19fb1ce07336effa8f2c7cee418a3d4f
Luke McHale-Jones <LukeMcJones@live.co.uk>
2017-06-19 11:42:35 +0100

    Added placeholders

--- client/src/components/admin/ingredient.js
+++ client/src/components/admin/ingredient.js
@@ -16,13 +16,11 @@ class Ingredient extends Component {
       <div className="form-group form-group-lg">
         <label className="control-label col-sm-2">Name </label>
         <div className="col-sm-3">
-          <input className="form-control" type="text" defaultValue={this.props.name} onChange={(e) => this.props.onIngredientFieldChange(e, this.props._id, 'name')} required maxLength="50" />      
-          <p className="validation-message valid">Ingredient name is required.</p>
+          <input className="form-control" placeholder="E.g. Vodka" type="text" defaultValue={this.props.name} onChange={(e) => this.props.onIngredientFieldChange(e, this.props._id, 'name')} required maxLength="50" />      
         </div>
         <label className="control-label col-sm-2">Amount </label>
-        <div className="col-sm-2">
-          <input className="form-control" type="number" min="0.5" max="5" step="0.5" defaultValue={this.props.amount} onChange={(e) => this.props.onIngredientFieldChange(e, this.props._id, 'amount')} required  maxLength="3" />        
-          <p className="validation-message valid">Amount is required.</p>
+        <div className="col-sm-3">
+          <input className="form-control" placeholder="E.g. 1.5" type="number" min="0.5" max="5" step="0.5" defaultValue={this.props.amount} onChange={(e) => this.props.onIngredientFieldChange(e, this.props._id, 'amount')} required  maxLength="3" />        
         </div>
         <div className="col-sm-1">
           {button}
